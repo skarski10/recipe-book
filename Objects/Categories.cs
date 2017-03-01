857ba83eb907753fb7c05fc3925b4ae6d2218516
Kory Skarbek and Marc Larkin <kory+marc@>
2017-03-01 11:34:37 -0800

    add tests for categories as well as functions

--- Objects/Categories.cs
+++ Objects/Categories.cs
@@ -57,6 +57,65 @@ namespace Cookbook
             }
         }
 
+        public void Save()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO categories(name) OUTPUT INSERTED.id VALUES (@CategoryName);", conn);
+
+            SqlParameter nameParameter = new SqlParameter("@CategoryName", this.GetCategoryName());
+
+            cmd.Parameters.Add(nameParameter);
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                this._id = rdr.GetInt32(0);
+            }
+            if (rdr != null)
+            {
+                rdr.Close();
+            }
+            if (conn != null)
+            {
+                conn.Close();
+            }
+        }
+
+
+
+        public static Category Find(int id)
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM categories WHERE id = @CategoryId;", conn);
+            SqlParameter CategoryIdParameter = new SqlParameter("@CategoryId", id.ToString());
+            cmd.Parameters.Add(CategoryIdParameter);
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            int foundCategoryId = 0;
+            string foundCategoryName = null;
+
+            while(rdr.Read())
+            {
+                foundCategoryId = rdr.GetInt32(0);
+                foundCategoryName = rdr.GetString(1);
+            }
+            Category foundCategory = new Category(foundCategoryName, foundCategoryId);
+
+            if(rdr != null)
+            {
+                rdr.Close();
+            }
+            if (conn != null)
+            {
+                conn.Close();
+            }
+            return foundCategory;
+        }
 
 
 
--- Tests/CategoriesTest.cs
+++ Tests/CategoriesTest.cs
@@ -9,7 +9,9 @@ namespace Cookbook
     public class CategoryTest : IDisposable
     {
         public static Category firstCategory = new Category("Dinner");
-        public static Recipe secondRecipe = new Recipe("Ham", "A Delicious Ham Sandwich", "Butter Bread. Add Ham. Add Cheese. Eat.");
+        public static Category secondCategory = new Category("Dinner");
+        public static Category thirdCategory = new Category("lunch");
+        public static Recipe firstRecipe = new Recipe("Ham", "A Delicious Ham Sandwich", "Butter Bread. Add Ham. Add Cheese. Eat.");
 
 
         public CategoryTest()
@@ -27,6 +29,56 @@ namespace Cookbook
             Assert.Equal(0,result);
         }
 
+        [Fact]
+        public void Test_EqualOverrideTrueForSameCategoryName()
+        {
+            //Arrange, Act, Assert
+            Assert.Equal(firstCategory, secondCategory);
+
+        }
+        [Fact]
+        public void Test_SaveToDatabase()
+        {
+            // Arrange
+            firstCategory.Save();
+
+            // Act
+            List<Category> result = Category.GetAllCategories();
+            List<Category> testList = new List<Category>{firstCategory};
+
+            // Assert
+            Assert.Equal(testList, result);
+        }
+
+        [Fact]
+        public void Test_SaveAssignsIdToObject()
+        {
+            //Arrange
+            firstCategory.Save();
+
+            //Act
+            Category testCategory = Category.GetAllCategories()[0];
+
+            int result = firstCategory.GetCategoryId();
+            int testId = testCategory.GetCategoryId();
+
+            //Assert
+            Assert.Equal(testId, result);
+        }
+
+
+        [Fact]
+        public void Test_Find_FindsCategoryinDatablase()
+        {
+            //Arrange
+            firstCategory.Save();
+            //Act
+            Category foundCategory = Category.Find(firstCategory.GetCategoryId());
+            Console.WriteLine(firstCategory.GetCategoryId());
+            Console.WriteLine(foundCategory.GetCategoryId());
+            //Asswert
+            Assert.Equal(firstCategory, foundCategory);
+        }
 
 
 
